29dab021d06894bcdaf34c7da94d270b6920580d
Lawrence <lawrencejengwa@gmail.com>
2020-09-26 12:38:05 +0200

    - map intent and phone added already

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -25,9 +25,6 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
     public void createMapIntent(View view){
         Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
@@ -37,9 +34,6 @@ public class InfoActivity extends AppCompatActivity {
         }
     }
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
     public void createPhoneIntent(View view) {
       Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
       phoneIntent.setData(Uri.parse("tel:0123456789"));
